520ed744594e0fd45fe6dd12449c845d6d85cc63
8141077 <random1246@mail.ru>
2016-11-12 23:45:51 +0300

    git-repository creation

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "В нашем магазине вы найдете бытовую технику по низким ценам!",
+   "keywords" => "бытовая техника, электроника, магазин",
+   "robots" => "index, follow"
+);
+?>
